24b6f1ad2828dc5b7733e9c9ef0241b57160f2e0
Rafael Alfaro <rafaela@thehangar.cr>
2018-03-26 16:44:33 -0600

    sets up canon ball basic movement

--- Assets/Project 1/Scripts/Cannonball.cs
+++ Assets/Project 1/Scripts/Cannonball.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cannonball : MonoBehaviour {
+	public int hitpoint = 20;
+	public float minForce = 400.0f;
+	public float maxForce = 700.0f;
+	public AudioClip audioHit = null;
+	public AudioClip audioShoot = null;
+	public ParticleSystem particle = null;
 
+	void Awake(){
+		this.GetComponent<Rigidbody> ().AddRelativeForce (new Vector3(0,400,400));
+	}
 }
