1177c10351e69dbddae97585b38443ba1f973ea6
Rafael Alfaro <rafaela@thehangar.cr>
2018-03-25 23:11:28 -0600

    declares main variables for MagicOrb script

--- Assets/Project 1/Scripts/MagicOrb.cs
+++ Assets/Project 1/Scripts/MagicOrb.cs
@@ -3,5 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MagicOrb : MonoBehaviour {
+	// speed, hitpoint, shoot sound, hit sound, particle
 
+	public int hitpoint = 10;
+	public float speed = 5.0f;
+	public AudioClip audioHit = null;
+	public AudioClip audioShoot = null;
+	public ParticleSystem particle = null;
 }
