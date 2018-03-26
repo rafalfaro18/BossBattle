d303477a5bfe91bcc27a1b22f2c1f13ef71ad6af
Rafael Alfaro <rafaela@thehangar.cr>
2018-03-26 17:06:58 -0600

    disables canonball particles after 2s

--- Assets/Project 1/Scripts/Cannonball.cs
+++ Assets/Project 1/Scripts/Cannonball.cs
@@ -6,9 +6,11 @@ public class Cannonball : MonoBehaviour {
 	public int hitpoint = 20;
 	public float minForce = 400.0f;
 	public float maxForce = 700.0f;
+	public float delayTime = 2.0f;
 	public AudioClip audioHit = null;
 	public AudioClip audioShoot = null;
 	public ParticleSystem particle = null;
+	private bool isActive = true;
 
 	void Awake(){
 		this.GetComponent<Rigidbody> ().AddRelativeForce (new Vector3(0,GetRandomValue(),GetRandomValue()));
@@ -17,5 +19,19 @@ public class Cannonball : MonoBehaviour {
 
 	float GetRandomValue(){
 		return Random.RandomRange (minForce, maxForce);
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if(isActive){
+			isActive = false;
+			this.GetComponent<AudioSource> ().PlayOneShot (audioShoot);
+			StartCoroutine (DisableParticle());
+		}
+	}
+
+	IEnumerator DisableParticle(){
+		yield return new WaitForSeconds (delayTime);
+		particle.enableEmission = false;
+		hitpoint = 0; //nothing can be hit by it
 	}
 }
