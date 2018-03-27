aac110e30c0d9e7c5c333db32f155b6da7637ca4
Rafael Alfaro <rafaela@thehangar.cr>
2018-03-27 08:59:42 -0600

    changes player color when hit

--- Assets/Project 1/Scripts/Hero.cs
+++ Assets/Project 1/Scripts/Hero.cs
@@ -10,7 +10,12 @@ public class Hero : MonoBehaviour {
 	public Transform socket = null;
 	public int magicOrbAmount = 20;
 	public int health = 100;
+	public Color hitColor = Color.white;
+	private Color originalColor = Color.white;
 
+	void Awake(){
+		originalColor = this.GetComponent<Renderer> ().material.color;
+	}
 
 	void Update(){
 		Move ();
@@ -44,9 +49,18 @@ public class Hero : MonoBehaviour {
 	void GetHealth(int hp){
 		if (health > 0) {
 			health -= hp;
+			if( hp > 0){
+				StartCoroutine (GetHit());
+			}
 			Debug.Log ("Hero health: " + health);
 		} else {
 			Debug.Log ("Game Over");
 		}
+	}
+
+	IEnumerator GetHit(){
+		this.GetComponent<Renderer> ().material.color = hitColor;
+		yield return new WaitForSeconds (0.4f);
+		this.GetComponent<Renderer> ().material.color = originalColor;
 	}
 }
