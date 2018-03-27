bb166a29de7bdd7891bd177fa9d5f90d36a4c16c
Rafael Alfaro <rafaela@thehangar.cr>
2018-03-27 07:57:05 -0600

    Turret follows (LookAt) player

--- Assets/Project 1/Scripts/Turret.cs
+++ Assets/Project 1/Scripts/Turret.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Turret : MonoBehaviour {
+	public GameObject canonBall = null;
+	public Transform player = null;
 
+	void Update(){
+		FollowPLayer ();
+	}
+
+	void FollowPLayer(){
+		this.transform.LookAt (player);
+	}
 }
