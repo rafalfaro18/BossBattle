fffb2aa5dae4329eff45c7056501f278442438d1
Rafael Alfaro <rafaela@thehangar.cr>
2018-03-27 08:04:50 -0600

    turrets shoots at player's direction

--- Assets/Project 1/Scripts/Turret.cs
+++ Assets/Project 1/Scripts/Turret.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class Turret : MonoBehaviour {
 	public GameObject canonBall = null;
 	public Transform player = null;
+	public float minDelay = 1.0f;
+	public float maxDelay = 4.0f;
+
+	private float lasTime = 0.0f;
+	private float delayTime = 0.0f;
 
 	void Update(){
 		FollowPLayer ();
+		Shoot ();
 	}
 
 	void FollowPLayer(){
 		this.transform.LookAt (player);
+	}
+
+	void Shoot(){
+		if(Time.time > lasTime + delayTime){
+			lasTime = Time.time;
+			delayTime = GetRandomValue ();
+			GameObject obj = Instantiate (canonBall, this.transform.position, this.transform.rotation) as GameObject;
+			obj.name = "canonball";
+		}
+	}
+
+	float GetRandomValue(){
+		return Random.Range (minDelay, maxDelay);
 	}
 }
