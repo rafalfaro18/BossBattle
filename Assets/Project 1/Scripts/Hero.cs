935e46aa853c5125b7e7eb17e4293aa9c305a5f7
Rafael Alfaro <rafaela@thehangar.cr>
2018-03-26 23:00:42 -0600

    player moves and shoots magic orb instances

--- Assets/Project 1/Scripts/Hero.cs
+++ Assets/Project 1/Scripts/Hero.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Hero : MonoBehaviour {
 
 	public float moveSpeed = 5.0f;
-	public float rotateSpeed = 5.0f;
-
+	public float rotateSpeed = 100.0f;
+	public GameObject magicOrb = null;
+	public Transform socket = null;
+	public int magicOrbAmount = 20;
 
 	void Update(){
 		Move ();
+		Shoot ();
 	}
 
 	void Move(){
@@ -17,5 +20,15 @@ public class Hero : MonoBehaviour {
 		float rotation = Input.GetAxis("Horizontal") * rotateSpeed;
 		this.transform.Translate( 0,0, move * Time.deltaTime);
 		this.transform.Rotate (0, rotation * Time.deltaTime, 0);
+	}
+
+	void Shoot(){
+		if(Input.GetButtonDown("Fire1")){ //Project Settings -> Input: ctrl, mouse or joystick button
+			if(magicOrbAmount > 0){
+				magicOrbAmount--;	
+				GameObject obj = Instantiate (magicOrb, socket.position, socket.rotation) as GameObject;
+				obj.name = "magicOrgb";
+			}
+		}
 	}
 }
