69c95e529fefbd8cbd2564344ad655fc9a87ef46
Rafael Alfaro <rafaela@thehangar.cr>
2018-03-26 22:27:49 -0600

    player movement

--- Assets/Project 1/Scripts/Hero.cs
+++ Assets/Project 1/Scripts/Hero.cs
@@ -4,4 +4,18 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour {
 
+	public float moveSpeed = 5.0f;
+	public float rotateSpeed = 5.0f;
+
+
+	void Update(){
+		Move ();
+	}
+
+	void Move(){
+		float move = Input.GetAxis("Vertical") * moveSpeed; //Input form keyboard ws, mouse or joystick
+		float rotation = Input.GetAxis("Horizontal") * rotateSpeed;
+		this.transform.Translate( 0,0, move * Time.deltaTime);
+		this.transform.Rotate (0, rotation * Time.deltaTime, 0);
+	}
 }
