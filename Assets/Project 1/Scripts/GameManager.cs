99f9850f428f861982798d19be2e6687949e3dc5
Rafael Alfaro <rafaela@thehangar.cr>
2018-03-27 09:32:17 -0600

    updates GUI with in game values

--- Assets/Project 1/Scripts/GameManager.cs
+++ Assets/Project 1/Scripts/GameManager.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Hero hero = null;
+	public Turret turret = null;
+
+	public Text playerHealth = null;
+	public Text magicOrbs = null;
+	public Text turretHealth = null;
+
+	void Update(){
+		DisplayProperties ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void DisplayProperties(){
+		playerHealth.text = "Player: " + hero.health.ToString ();
+		magicOrbs.text = "MagicOrbs: " + hero.magicOrbAmount.ToString ();
+		turretHealth.text = "Turret: " + turret.health.ToString ();
+
 	}
 }
